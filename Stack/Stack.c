6863f76908d364e50762505b34ce61d157becd54
Arthur <arthur123ruan@hotmail.com>
2019-12-10 23:53:43 -0300

    Q1

--- Stack/Stack.c
+++ Stack/Stack.c
@@ -21,29 +21,3 @@ char Pop(t_stack *s){
         exit(1);
     }
 }
-/*
-int main(void){
-
-    t_stack s = {.top = -1}; // iniciallization top
-    int i = 0;
-
-    Push(&s, 'S');    
-    Push(&s, 'T');    
-    Push(&s, 'A');    
-    Push(&s, 'C');    
-    Push(&s, 'K');    
-    Push(&s, 'A');
-
-    Pop(&s);
-    Pop(&s);
-   
-    Push(&s, 'K');    
-
-    for(i = 0; i < STACK_SIZE; i++){
-        printf("%c", s.v[i]);
-    }
-
-    printf("\n%i\n", s.top);
-
-    return 0;
-}*/
